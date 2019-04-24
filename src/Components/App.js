81686cf9ea212dd4fed617e5dab6cbafa8f43c3c
Kyle C <kyclutter@gmail.com>
2019-04-24 14:19:44 -0500

    update

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,29 @@
-import React, { Component } from 'react';
-
+import React from 'react';
+import { Grid } from 'semantic-ui-react'
+import ColorPanel from './ColorPanel/ColorPanel';
+import Messages from './Messages/Messages';
+import MettaPanel from './MetaPanel/MetaPanel';
+import SidePanel from './SidePanel/SidePanel';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div >
-        App
-      </div>
-    );
-  }
+
+const App = ()=> {
+  return(
+    <Grid columns="equal" className="app" style={{background: '#eee'}}>
+      <ColorPanel/>
+      <SidePanel />
+      <Grid.Column style={{marginLeft: 320}}>
+        <Messages />
+      </Grid.Column>
+      <Grid.Column width={4}>
+        <MettaPanel />
+      </Grid.Column>
+      
+  </Grid>
+
+  )
+    
 }
+
 
 export default App;
---
+++ src/Components/SidePanel/SidePanel.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class SidePanel extends Component {
+
+
+    render(){
+
+        return(
+            <div>
+                Side Panel
+            </div>
+
+        )
+    }
+}
+
+
+
+export default SidePanel;
